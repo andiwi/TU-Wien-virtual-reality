cb70bff96601e5682e92d83c240b34e865e96d5d
Michael Wagner <mwagnurr@gmail.com>
2016-10-12 12:30:42 +0200

    adds second scene and stuff

---
+++ ass1_LeapTask/Assets/Resources/Scripts/CreateCube.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateCube : MonoBehaviour {
+
+    public GameObject gameObject;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            
+        }
+	}
+
+
+    public void CreateGameObjectInstance()
+    {
+        gameObject.transform.position = new Vector3(1, 1, 1);
+        Instantiate(gameObject);
+        Debug.Log("CreateGameObjectInstance called!");
+    }
+}
