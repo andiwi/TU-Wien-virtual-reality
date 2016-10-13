e4da2e7b8fb7385c7e06917e56d3003045919da6
Andreas Wittmann <andreas.wittann.aw@gmail.com>
2016-10-13 12:31:18 +0200

    enhancement of cube creation

--- ass1_LeapTask/Assets/Resources/Scripts/CubeFactory.cs
+++ ass1_LeapTask/Assets/Resources/Scripts/CubeFactory.cs
@@ -3,7 +3,10 @@ using System.Collections;
 using Leap.Unity;
 
 public class CubeFactory : MonoBehaviour {
-	
+
+	public float scaleFactor; //recommended: between 0 and 1
+	public float accuracy_threshold; //only distance changes higher than this threshold will result in cube size changes.
+
 	[SerializeField]
 	private PinchDetector _pinchDetectorL;
 	public PinchDetector PinchDetectorL {
@@ -35,55 +38,48 @@ public class CubeFactory : MonoBehaviour {
 	private float distance;
 	private Vector3 maxScaleSize = new Vector3(5,5,5);
 	private Vector3 minScaleSize = new Vector3(0.5f, 0.5f, 0.5f);
-	public float scaleFactor;
 
 	// Use this for initialization
 	void Start () {
-		/*
 		cubeCreationRunning = true;
 
 		cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		cube.AddComponent<Rigidbody>();
 		cube.transform.position = new Vector3(1, 1, 1);
-
-		cube.GetComponent<Rigidbody> ().useGravity = false;
-		*/
 	}
 
 	public void StartCubeCreation() {
 		cubeCreationRunning = true;
 
 		cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		cube.AddComponent<Rigidbody>();
 		cube.transform.position = (_pinchDetectorL.Position + _pinchDetectorR.Position) / 2.0f;
-
-		cube.GetComponent<Rigidbody> ().useGravity = false;
 	}
 
 	public void EndCubeCreation() {
 		cubeCreationRunning = false;
-		cube.GetComponent<Rigidbody> ().useGravity = true;
+		cube.AddComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (cubeCreationRunning == true) {
-			if (_pinchDetectorL.DidChangeFromLastFrame || _pinchDetectorR.DidChangeFromLastFrame) {
-				float newDistance = Vector3.Distance (_pinchDetectorL.Position, _pinchDetectorR.Position);
+			
+			float newDistance = Vector3.Distance (_pinchDetectorL.Position, _pinchDetectorR.Position);
+			float deltaDistance = (newDistance - distance) * scaleFactor;
 
-				if (newDistance > distance
-					&& cube.transform.localScale.x < maxScaleSize.x
-					&& cube.transform.localScale.y < maxScaleSize.y
-					&& cube.transform.localScale.z < maxScaleSize.z) {
-					cube.transform.localScale += Vector3.one * scaleFactor;
-				} else if (newDistance < distance
-					&& cube.transform.localScale.x > minScaleSize.x
-					&& cube.transform.localScale.y > minScaleSize.y
-					&& cube.transform.localScale.z > minScaleSize.z) {
-					cube.transform.localScale -= Vector3.one * scaleFactor;
-				}
+			if (deltaDistance > accuracy_threshold
+				&& cube.transform.localScale.x < maxScaleSize.x
+				&& cube.transform.localScale.y < maxScaleSize.y
+				&& cube.transform.localScale.z < maxScaleSize.z) {
+				cube.transform.localScale += (Vector3.one * deltaDistance);
+				distance = newDistance;
+			} else if (deltaDistance < -accuracy_threshold
+				&& cube.transform.localScale.x > minScaleSize.x
+				&& cube.transform.localScale.y > minScaleSize.y
+				&& cube.transform.localScale.z > minScaleSize.z) {
+				cube.transform.localScale += (Vector3.one * deltaDistance);
 				distance = newDistance;
 			}
+			
 
 			/*
 			//
@@ -91,7 +87,8 @@ public class CubeFactory : MonoBehaviour {
 			//
 			if (Input.GetKeyDown ("q")) {
 				cubeCreationRunning = false;
-				cube.GetComponent<Rigidbody> ().useGravity = true;
+				cube.AddComponent<Rigidbody>();
+				//cube.GetComponent<Rigidbody> ().useGravity = true;
 			}
 						 
 			Vector3 lpos = new Vector3();
@@ -111,27 +108,29 @@ public class CubeFactory : MonoBehaviour {
 
 			if (lposSet == true && rposSet == true) {
 				float newDistance = Vector3.Distance (lpos, rpos);
-				Debug.Log ("old distance: " + distance);
-				Debug.Log ("new distance: " + newDistance);
 
-				Vector3 scaleFactor = new Vector3 (0.1f, 0.1f, 0.1f);
-				if (newDistance > distance
+				float deltaDistance = (newDistance - distance) * scaleFactor;
+				Debug.Log (deltaDistance);
+
+				if (deltaDistance > accuracy_threshold
 					&& cube.transform.localScale.x < maxScaleSize.x
 					&& cube.transform.localScale.y < maxScaleSize.y
 					&& cube.transform.localScale.z < maxScaleSize.z) {
-					cube.transform.localScale += scaleFactor;
-				} else if (newDistance < distance
+						cube.transform.localScale += (Vector3.one * deltaDistance);
+						distance = newDistance;
+				} else if (deltaDistance < -accuracy_threshold
 					&& cube.transform.localScale.x > minScaleSize.x
 					&& cube.transform.localScale.y > minScaleSize.y
 					&& cube.transform.localScale.z > minScaleSize.z) {
-					cube.transform.localScale -= scaleFactor;
+						cube.transform.localScale += (Vector3.one * deltaDistance);
+						distance = newDistance;
 				}
-				distance = newDistance;
 
 				lposSet = false;
 				rposSet = false;
 			}
 			*/
+
 		}
 	}
 }
