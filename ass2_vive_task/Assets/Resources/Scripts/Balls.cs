579aa3b6ea5f896b9a65d1167ec41c82f173e48c
Andreas Wittmann <andreas.wittann.aw@gmail.com>
2016-10-19 18:19:15 +0200

    box collision

--- ass2_vive_task/Assets/Resources/Scripts/Balls.cs
+++ ass2_vive_task/Assets/Resources/Scripts/Balls.cs
@@ -27,7 +27,6 @@ public class Balls : MonoBehaviour {
 		cue_ball.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		cue_ball.transform.position = new Vector3 (0, 0.5f, -1.5f);
 		cue_ball.GetComponent<Renderer> ().sharedMaterial = ball0_material;
-
 		Rigidbody rb = cue_ball.AddComponent<Rigidbody>();
 		rb.mass = 2;
 		rb.useGravity = false;
@@ -35,6 +34,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball1 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball1.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball1.transform.position = new Vector3 (-0.5f, 0, -0.5f);
+		ball1.tag = "Ball";
 		//Material ball1_material= Resources.Load("Resources/Materials/Ball_01.mat", typeof(Material)) as Material;
 		ball1.GetComponent<Renderer> ().sharedMaterial = ball1_material;
 		Rigidbody rb1 = ball1.AddComponent<Rigidbody>();
@@ -43,6 +43,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball2 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball2.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball2.transform.position = new Vector3 (-0.5f, 0, 0);
+		ball2.tag = "Ball";
 		ball2.GetComponent<Renderer> ().sharedMaterial = ball2_material;
 		Rigidbody rb2 = ball2.AddComponent<Rigidbody>();
 		rb2.useGravity = false;
@@ -50,6 +51,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball3 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball3.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball3.transform.position = new Vector3 (0.5f, 0, 0);
+		ball3.tag = "Ball";
 		ball3.GetComponent<Renderer> ().sharedMaterial = ball3_material;
 		Rigidbody rb3 = ball3.AddComponent<Rigidbody>();
 		rb3.useGravity = false;
@@ -57,6 +59,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball4 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball4.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball4.transform.position = new Vector3 (0, 0, 0.5f);
+		ball4.tag = "Ball";
 		ball4.GetComponent<Renderer> ().sharedMaterial = ball4_material;
 		Rigidbody rb4 = ball4.AddComponent<Rigidbody>();
 		rb4.useGravity = false;
@@ -64,6 +67,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball5 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball5.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball5.transform.position = new Vector3 (-0.5f, 0, 0.5f);
+		ball5.tag = "Ball";
 		ball5.GetComponent<Renderer> ().sharedMaterial = ball5_material;
 		Rigidbody rb5 = ball5.AddComponent<Rigidbody>();
 		rb5.useGravity = false;
@@ -71,6 +75,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball6 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball6.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball6.transform.position = new Vector3 (0.5f, 0, 0.5f);
+		ball6.tag = "Ball";
 		ball6.GetComponent<Renderer> ().sharedMaterial = ball6_material;
 		Rigidbody rb6 = ball6.AddComponent<Rigidbody>();
 		rb6.useGravity = false;
@@ -78,6 +83,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball7 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball7.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball7.transform.position = new Vector3 (0, 0, -0.5f);
+		ball7.tag = "Ball";
 		ball7.GetComponent<Renderer> ().sharedMaterial = ball7_material;
 		Rigidbody rb7 = ball7.AddComponent<Rigidbody>();
 		rb7.useGravity = false;
@@ -85,6 +91,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball8 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball8.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball8.transform.position = new Vector3 (0, 0, 0);
+		ball8.tag = "Ball";
 		ball8.GetComponent<Renderer> ().sharedMaterial = ball8_material;
 		Rigidbody rb8 = ball8.AddComponent<Rigidbody>();
 		rb8.useGravity = false;
@@ -92,6 +99,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball9 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball9.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball9.transform.position = new Vector3 (0.5f, 0, -0.5f);
+		ball9.tag = "Ball";
 		ball9.GetComponent<Renderer> ().sharedMaterial = ball9_material;
 		Rigidbody rb9 = ball9.AddComponent<Rigidbody>();
 		rb9.useGravity = false;
@@ -99,6 +107,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball10 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball10.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball10.transform.position = new Vector3 (-0.25f, 0.38f, -0.25f);
+		ball10.tag = "Ball";
 		ball10.GetComponent<Renderer> ().sharedMaterial = ball10_material;
 		Rigidbody rb10 = ball10.AddComponent<Rigidbody>();
 		rb10.useGravity = false;
@@ -106,6 +115,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball11 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball11.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball11.transform.position = new Vector3 (0.25f, 0.38f, -0.25f);
+		ball11.tag = "Ball";
 		ball11.GetComponent<Renderer> ().sharedMaterial = ball11_material;
 		Rigidbody rb11 = ball11.AddComponent<Rigidbody>();
 		rb11.useGravity = false;
@@ -113,6 +123,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball12 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball12.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball12.transform.position = new Vector3 (-0.25f, 0.38f, 0.25f);
+		ball12.tag = "Ball";
 		ball12.GetComponent<Renderer> ().sharedMaterial = ball12_material;
 		Rigidbody rb12 = ball12.AddComponent<Rigidbody>();
 		rb12.useGravity = false;
@@ -120,6 +131,7 @@ public class Balls : MonoBehaviour {
 		GameObject ball13 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball13.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball13.transform.position = new Vector3 (0.25f, 0.38f, 0.25f);
+		ball13.tag = "Ball";
 		ball13.GetComponent<Renderer> ().sharedMaterial = ball13_material;
 		Rigidbody rb13 = ball13.AddComponent<Rigidbody>();
 		rb13.useGravity = false;
@@ -127,14 +139,17 @@ public class Balls : MonoBehaviour {
 		GameObject ball14 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		ball14.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
 		ball14.transform.position = new Vector3 (0, 0.76f, 0);
+		ball14.tag = "Ball";
 		ball14.GetComponent<Renderer> ().sharedMaterial = ball14_material;
 		Rigidbody rb14 = ball14.AddComponent<Rigidbody>();
 		rb14.useGravity = false;
+
+		cue_ball.GetComponent<Rigidbody> ().AddForce (transform.forward * 1000);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		cue_ball.GetComponent<Rigidbody> ().AddForce (transform.forward * 10);
+		
 	}
 }
--- ass2_vive_task/Assets/Resources/Scripts/GamingBox.cs
+++ ass2_vive_task/Assets/Resources/Scripts/GamingBox.cs
@@ -11,63 +11,153 @@ public class GamingBox : MonoBehaviour {
 		box1.transform.localScale = new Vector3 (7, 7, 0.1f);
 		box1.transform.position = new Vector3(0, 0,-3.5f);
 		box1.GetComponent<Renderer>().sharedMaterial = boxMaterial;
+		Rigidbody rb = box1.AddComponent<Rigidbody>();
+		rb.constraints = RigidbodyConstraints.FreezeAll;
+		box1.AddComponent<BoxCollision> ();
 
 		GameObject box2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		box2.transform.localScale = new Vector3 (7, 7, 0.1f);
 		box2.transform.position = new Vector3(0, 0, 3.5f);
 		box2.GetComponent<Renderer>().sharedMaterial = boxMaterial;
+		Rigidbody rb2 = box2.AddComponent<Rigidbody>();
+		rb2.constraints = RigidbodyConstraints.FreezeAll;
+		box2.AddComponent<BoxCollision> ();
 
 		GameObject box3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		box3.transform.localScale = new Vector3 (7, 0.1f, 7);
 		box3.transform.position = new Vector3(0, 3.5f, 0);
 		box3.GetComponent<Renderer>().sharedMaterial = boxMaterial;
+		Rigidbody rb3 = box3.AddComponent<Rigidbody>();
+		rb3.constraints = RigidbodyConstraints.FreezeAll;
+		box3.AddComponent<BoxCollision> ();
 
 		GameObject box4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		box4.transform.localScale = new Vector3 (7, 0.1f, 7);
 		box4.transform.position = new Vector3(0, -3.5f, 0);
 		box4.GetComponent<Renderer>().sharedMaterial = boxMaterial;
+		Rigidbody rb4 = box4.AddComponent<Rigidbody>();
+		rb4.constraints = RigidbodyConstraints.FreezeAll;
+		box4.AddComponent<BoxCollision> ();
 
 		GameObject box5 = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		box5.transform.localScale = new Vector3 (0.1f, 7, 7);
 		box5.transform.position = new Vector3(-3.5f, 0, 0);
 		box5.GetComponent<Renderer>().sharedMaterial = boxMaterial;
+		Rigidbody rb5 = box5.AddComponent<Rigidbody>();
+		rb5.constraints = RigidbodyConstraints.FreezeAll;
+		box5.AddComponent<BoxCollision> ();
 
 		GameObject box6 = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		box6.transform.localScale = new Vector3 (0.1f, 7, 7);
 		box6.transform.position = new Vector3(3.5f, 0, 0);
 		box6.GetComponent<Renderer>().sharedMaterial = boxMaterial;
+		Rigidbody rb6 = box6.AddComponent<Rigidbody>();
+		rb6.constraints = RigidbodyConstraints.FreezeAll;
+		box6.AddComponent<BoxCollision> ();
 
+		//holes
 		GameObject hole1 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		hole1.transform.position = new Vector3 (-3.5f, -3.5f, -3.5f);
 		hole1.GetComponent<Renderer>().sharedMaterial = holeMaterial;
+		Rigidbody rb_hole1 = hole1.AddComponent<Rigidbody>();
+		rb_hole1.constraints = RigidbodyConstraints.FreezeAll;
 
 		GameObject hole2 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		hole2.transform.position = new Vector3 (-3.5f, -3.5f, 3.5f);
 		hole2.GetComponent<Renderer>().sharedMaterial = holeMaterial;
+		Rigidbody rb_hole2 = hole2.AddComponent<Rigidbody>();
+		rb_hole2.constraints = RigidbodyConstraints.FreezeAll;
 
 		GameObject hole3 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		hole3.transform.position = new Vector3 (-3.5f, 3.5f, -3.5f);
 		hole3.GetComponent<Renderer>().sharedMaterial = holeMaterial;
+		Rigidbody rb_hole3 = hole3.AddComponent<Rigidbody>();
+		rb_hole3.constraints = RigidbodyConstraints.FreezeAll;
 
 		GameObject hole4 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		hole4.transform.position = new Vector3 (-3.5f, 3.5f, 3.5f);
 		hole4.GetComponent<Renderer>().sharedMaterial = holeMaterial;
+		Rigidbody rb_hole4 = hole4.AddComponent<Rigidbody>();
+		rb_hole4.constraints = RigidbodyConstraints.FreezeAll;
 
 		GameObject hole5 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		hole5.transform.position = new Vector3 (3.5f, -3.5f, -3.5f);
 		hole5.GetComponent<Renderer>().sharedMaterial = holeMaterial;
+		Rigidbody rb_hole5 = hole5.AddComponent<Rigidbody>();
+		rb_hole5.constraints = RigidbodyConstraints.FreezeAll;
 
 		GameObject hole6 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		hole6.transform.position = new Vector3 (3.5f, -3.5f, 3.5f);
 		hole6.GetComponent<Renderer>().sharedMaterial = holeMaterial;
+		Rigidbody rb_hole6 = hole6.AddComponent<Rigidbody>();
+		rb_hole6.constraints = RigidbodyConstraints.FreezeAll;
 
 		GameObject hole7 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		hole7.transform.position = new Vector3 (3.5f, 3.5f, -3.5f);
 		hole7.GetComponent<Renderer>().sharedMaterial = holeMaterial;
+		Rigidbody rb_hole7 = hole7.AddComponent<Rigidbody>();
+		rb_hole7.constraints = RigidbodyConstraints.FreezeAll;
 
 		GameObject hole8 = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		hole8.transform.position = new Vector3 (3.5f, 3.5f, 3.5f);
 		hole8.GetComponent<Renderer>().sharedMaterial = holeMaterial;
+		Rigidbody rb_hole8 = hole8.AddComponent<Rigidbody>();
+		rb_hole8.constraints = RigidbodyConstraints.FreezeAll;
+
+
+		//ignore collisions between gaming box objects
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), box2.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), box3.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), box4.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), box5.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), box6.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box2.GetComponent<Collider> (), box3.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box2.GetComponent<Collider> (), box4.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box2.GetComponent<Collider> (), box5.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box2.GetComponent<Collider> (), box6.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box3.GetComponent<Collider> (), box4.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box3.GetComponent<Collider> (), box5.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box3.GetComponent<Collider> (), box6.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box4.GetComponent<Collider> (), box5.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box4.GetComponent<Collider> (), box6.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box5.GetComponent<Collider> (), box6.GetComponent<Collider> ());
+
+
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), hole1.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box4.GetComponent<Collider> (), hole1.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box5.GetComponent<Collider> (), hole1.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box2.GetComponent<Collider> (), hole2.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box4.GetComponent<Collider> (), hole2.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box5.GetComponent<Collider> (), hole2.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), hole3.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box3.GetComponent<Collider> (), hole3.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box5.GetComponent<Collider> (), hole3.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box2.GetComponent<Collider> (), hole4.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box3.GetComponent<Collider> (), hole4.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box5.GetComponent<Collider> (), hole4.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), hole5.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box4.GetComponent<Collider> (), hole5.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box6.GetComponent<Collider> (), hole5.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box2.GetComponent<Collider> (), hole6.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box4.GetComponent<Collider> (), hole6.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box6.GetComponent<Collider> (), hole6.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box1.GetComponent<Collider> (), hole7.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box3.GetComponent<Collider> (), hole7.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box6.GetComponent<Collider> (), hole7.GetComponent<Collider> ());
+
+		Physics.IgnoreCollision (box2.GetComponent<Collider> (), hole8.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box3.GetComponent<Collider> (), hole8.GetComponent<Collider> ());
+		Physics.IgnoreCollision (box6.GetComponent<Collider> (), hole8.GetComponent<Collider> ());
 
 	}
 	
