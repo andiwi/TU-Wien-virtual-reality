b939027051290cb5bca95a99d3cc9f567e4086f8
Andreas Wittmann <andreas.wittann.aw@gmail.com>
2016-11-08 11:14:55 +0100

    ongrabbedbehaviour

--- ass3_network_task/Task3/Assets/Resources/Scripts/OnGrabbedBehaviour.cs
+++ ass3_network_task/Task3/Assets/Resources/Scripts/OnGrabbedBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Leap.Unity;
 
 // TODO: define the behaviour of a shared object when it is manipulated by a client
 
@@ -7,10 +8,37 @@ public class OnGrabbedBehaviour : MonoBehaviour {
 
    
     bool grabbed;
+	private bool vive;
+	private bool leap;
+
+	private GameObject playerController;
+
+	private PinchDetector pinchDetectorL;
+	private PinchDetector pinchDetectorR;
 
     // Use this for initialization
     void Start () {
+		GameObject capsuleHand_L = GameObject.Find("CapsuleHand_L");
+		if (capsuleHand_L != null) {
+			leap = true;
+			vive = false;
 
+			GameObject capsuleHand_R = GameObject.Find ("CapsuleHand_R");
+
+			pinchDetectorL = capsuleHand_L.GetComponent<PinchDetector> ();
+			pinchDetectorR = capsuleHand_R.GetComponent<PinchDetector> ();
+
+		} else {
+			GameObject controller_L = GameObject.Find ("Controller (left)");
+			if (controller_L != null) {
+				leap = false;
+				vive = true;
+
+				GameObject controller_R = GameObject.Find ("Controller (right)");
+
+				//TODO
+			}
+		}
 	}
 	
 	// Update is called once per frame
@@ -19,21 +47,24 @@ public class OnGrabbedBehaviour : MonoBehaviour {
         // GO´s behaviour when it is in a grabbed state (owned by a client) should be defined here
         if(grabbed)
         {
-            
+			Debug.Log ("grabbed");
+			if (leap) {
+				transform.parent.gameObject.transform.position = (pinchDetectorL.Position + pinchDetectorR.Position) / 2f;
+			} else if (vive) {
+				//TODO
+			}
         }
 	}
 
     // called first time when the GO gets grabbed by a player
-    public void OnGrabbed(Transform test)
+    public void OnGrabbed()
     {
-        transform.SetParent(test);
-       
+		grabbed = true;
     }
 
     // called when the GO gets released by a player
     public void OnReleased()
     {
-        transform.SetParent(null);
-
+		grabbed = false;
     }
 }
