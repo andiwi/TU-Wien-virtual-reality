ff441aa6a289dd67a08453d834a24ebd55232050
Maxim <Maxim>
2016-12-12 15:35:05 +0100

    snowballcollision

---
+++ project_snow/Assets/Resources/Scripts/Gameplay/SnowBallCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowBallCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("OnCollisionEnter Snowball / switching tag");
+        gameObject.tag = "enemySnowballDeactivated";
+    }
+}
